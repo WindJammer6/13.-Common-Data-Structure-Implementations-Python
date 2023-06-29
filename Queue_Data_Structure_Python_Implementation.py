c0db83c095922d51ed290768cc64a0601d06d762
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-06-30 00:58:17 +0800

    Create Queue_Data_Structure_Python_Implementation.py

---
+++ Queue_Data_Structure_Python_Implementation.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self,value):
+        self.buffer.appendleft(value)
+
+    def dequeue(self):
+        return self.buffer.pop()
+    
+    def is_empty(self):
+        return len(self.buffer) == 0
+    
+    def size(self):
+        return len(self.buffer)
+    
+    def __repr__(self):
+        return '{}'.format(self.buffer)
+    
+
+if __name__ == '__main__':
+    queue = Queue()
+
+    queue.enqueue({    
+        'company':'Walmart',
+        'timestamp': '15 apr, 11.01am',
+        'price': 131.10
+    })
+
+    print(queue.dequeue())
+    print(queue)
+
+    print(queue.is_empty()) 
+
+    queue.enqueue({    
+        'company':'Walmart',
+        'timestamp': '15 apr, 11.01am',
+        'price': 131.10
+    })
+    queue.enqueue({    
+        'company':'Walmart',
+        'timestamp': '15 apr, 11.02am',
+        'price': 131.12
+    })
+    queue.enqueue({    
+        'company':'Walmart',
+        'timestamp':'15 apr, 11.03am',
+        'price': 135
+    })
+    print(queue.dequeue())
+    print(queue.size())
+    print(queue)
