d2295fa8f0a3b6b905b63fae09db91e724026515
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-07-22 15:23:00 +0800

    Update 5. Queue.py

--- 5. Queue.py
+++ 5. Queue.py
@@ -9,18 +9,18 @@ class Queue:
 
     def dequeue(self):
         return self.buffer.pop()
-    
-    def is_empty(self):
-        return len(self.buffer) == 0
-    
-    def size(self):
-        return len(self.buffer)
 
     def front_element(self):
         return self.buffer[-1]
     
     def last_element(self):
         return self.buffer[0]
+    
+    def is_empty(self):
+        return len(self.buffer) == 0
+    
+    def size(self):
+        return len(self.buffer)
     
     def __repr__(self):
         return '{}'.format(self.buffer)
